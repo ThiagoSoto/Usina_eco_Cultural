68132e39426b31978b4ab1db454d377de6ac74a9
ThiagoSoto <thiagoaugusto979505@gmail.com>
2023-10-29 17:15:40 -0300

    1.0.3

--- index.js
+++ index.js
@@ -1,47 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const images = document.querySelectorAll(".banners img");
-    const indicatorsContainer = document.querySelector(".indicadores");
-    let currentIndex = 0;
+var radio = document.querySelector('.manual.btn');
+var cont = 1;
 
-    function showImage(index) {
-        images.forEach((img, i) => {
-            if (i === index) {
-                img.style.display = "block";
-            } else {
-                img.style.display = "none";
-            }
-        });
+document.getElementById('radio1').checked = true;
 
-        // Atualiza o indicador ativo
-        const indicators = document.querySelectorAll(".indicator");
-        indicators.forEach((indicator, i) => {
-            if (i === index) {
-                indicator.classList.add("active");
-            } else {
-                indicator.classList.remove("active");
-            }
-        });
+setInterval(() => {
+    proximaImg()
+}, 3000);
+
+function proximaImg(){
+    cont++;
+    if (cont > 3){
+        cont = 1 ;
     }
 
-    function nextImage() {
-        currentIndex = (currentIndex + 1) % images.length;
-        showImage(currentIndex);
-    }
-
-    setInterval(nextImage, 3000); // Alterna a cada 3 segundos (3000 milissegundos)
-
-    // Cria os indicadores
-    images.forEach(() => {
-        const indicator = document.createElement("div");
-        indicator.classList.add("indicator");
-        indicatorsContainer.appendChild(indicator);
-
-        indicator.addEventListener("click", function () {
-            currentIndex = Array.from(indicatorsContainer.children).indexOf(indicator);
-            showImage(currentIndex);
-        });
-    });
-
-    // Mostra a primeira imagem ao carregar a página
-    showImage(currentIndex);
-});
+    document.getElementById('radio'+ cont).checked = true;
+}
